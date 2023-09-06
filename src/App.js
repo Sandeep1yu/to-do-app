35152cff5d9845bc9b6570fe09cf76d7c94eebc0
Sandeep <sandeep167kumar@gmail.com>
2023-09-06 08:20:42 +0530

    added first all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Button, InputLabel, FormControl, TextField } from '@react/core';
+
+// import { Button } from "@mui/material/Button";
+// import { InputLabel } from "@mui/material/InputLabel";
+// import {  FormControl} from "@mui/material/FormControl";
+// import {  TextField} from "@mui/material/TextField";
+
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Take dogs for a walk",
+    "Take the rubbish out",
+  ]);
+  const [input, setInput] = useState("");
+
+  const eventHandler = (event) => {
+    setInput(event.target.value);
+  };
+  const todoHandler = (event) => {
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Hello Sandeep Programmar</h1>
+      <form>
+        <FormControl>
+          <InputLabel>Write a todo</InputLabel>
+          <TextField id="standard-basic" value={input} label="Standard" variant="standard" onChange={eventHandler}/>
+          
+        </FormControl>
+        <Button
+          disabled={!input}
+          type="submit"
+          variant="contained"
+          onClick={todoHandler}
         >
-          Learn React
-        </a>
-      </header>
+          Add Todo
+        </Button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
