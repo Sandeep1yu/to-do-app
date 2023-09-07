dd7c348e7f7c5aba11329ff0ae8a4f7edc91ee00
Sandeep <sandeep167kumar@gmail.com>
2023-09-07 12:26:22 +0530

    deleted some unnecessry imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { useState } from "react";
-// import  Button from "@mui/material/Button";
-// import  InputLabel from "@mui/material/InputLabel";
-// import  FormControl from "@mui/material/FormControl";
-// import  Input from "@mui/material/Input";
 import Todo from "./Todo";
 import { Button, InputLabel, FormControl, Input } from "@mui/material";
-
 import "./App.css";
+
+
 
 function App() {
   const [todos, setTodos] = useState([
@@ -29,8 +26,12 @@ function App() {
       <form>
         <FormControl>
           <InputLabel>Write a todo</InputLabel>
-          <Input id="standard-basic" value={input}  variant="standard" onChange={eventHandler}/>
-          
+          <Input
+            id="standard-basic"
+            value={input}
+            variant="standard"
+            onChange={eventHandler}
+          />
         </FormControl>
         <Button
           disabled={!input}
@@ -44,7 +45,7 @@ function App() {
 
       <ul>
         {todos.map((todo) => (
-         <Todo text={todo}/>
+          <Todo text={todo} />
         ))}
       </ul>
     </div>
